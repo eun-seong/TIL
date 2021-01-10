1cd17273d387a43ae007aabbbe6904f19c29caea
eun-seong <eunseongofficial@gmail.com>
2021-01-10 22:02:14 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `dev_eun`,
+    description: `오늘보다 발전된 내일을 만들기 위한 dev_eun의 TIL 블로그`,
+    author: `@eun-seong`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
