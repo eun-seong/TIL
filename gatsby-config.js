ff014ce95dd73115870bd049779be2cccf6eb8a4
eunseonglee <eunseongofficial@gmail.com>
2022-01-10 05:53:29 +0900

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,97 +1,44 @@
-const path = require('path');
-
 module.exports = {
-  pathPrefix: '/TIL',
   siteMetadata: {
-    title: `dev_eun`,
-    description: `오늘보다 발전된 내일을 만들기 위한 dev_eun의 TIL 블로그`,
-    author: `@eun-seong`,
-    siteUrl: `https://eun-seong.github.io/TIL/`,
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'gatsby-start-til',
   },
   plugins: [
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-typescript`,
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-vscode`,
-            options: {
-              colorTheme: 'Dark+ (default dark)',
-              injectStyles: true,
-              extensions: [],
-              extensionDataDirectory: path.resolve('extensions'),
-              logLevel: 'error',
-            },
-          },
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-        ],
+        icon: 'src/images/icon.png',
       },
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: './src/images/',
       },
+      __key: 'images',
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'posts',
-        path: `${__dirname}/posts`,
+        name: 'pages',
+        path: './src/pages/',
       },
+      __key: 'pages',
     },
     {
-      resolve: `gatsby-plugin-generate-typings`,
+      resolve: `gatsby-plugin-typescript`,
       options: {
-        dest: `./src/graphql-types.d.ts`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon-512x512.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ['G-T4NZQC7Q38'],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ['/preview/**', '/do-not-track/me/too/'],
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: 'https://eun-seong.github.io/TIL',
-        sitemap: 'https://eun-seong.github.io/TIL/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }],
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
       },
     },
   ],
--- src/interface.ts
+++
@@ -1,7 +0,0 @@
-import {ReplaceComponentRendererArgs} from 'gatsby';
-
-export type ITemplateProps<T> = ReplaceComponentRendererArgs['props'] & {
-    pageContext: {
-        isCreatedByStatefulCreatePages: boolean;
-    } & T;
-};
