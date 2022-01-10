ca15e575ea7c01b6874bbc97d26f3c611f766482
eunseonglee <eunseongofficial@gmail.com>
2022-01-11 03:27:02 +0900

    feat: 변경내역 가져오는 훅 구현

---
+++ src/hooks/useCommitDiff.ts
@@ -0,0 +1,27 @@
+import { CommitDiff } from '../types';
+import useLazyFetch from './useLazyFetch';
+
+const useCommitDiff = () => {
+  const { fetch: lazyFetch, data, isLoading, error } = useLazyFetch<CommitDiff[]>();
+
+  return {
+    fetch: ({ from, to }: { from: string; to: string }) =>
+      lazyFetch({
+        url: `/${process.env.GATSBY_AUTHOR}/${process.env.GATSBY_REPO}/compare/${from}...${to}`,
+        transformResponse: (res) =>
+          JSON.parse(res)
+            .files.filter((file) => !!file.filename.match(/^contents/))
+            .map((file) => ({
+              sha: file.sha,
+              filename: file.filename,
+              status: file.status,
+              patch: file.patch,
+            })),
+      }),
+    data,
+    isLoading,
+    error,
+  };
+};
+
+export default useCommitDiff;
