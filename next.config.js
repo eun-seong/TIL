c6e6bffe851876be372b183510518d738ec80951
eun-seong <eunseong.dev@gmail.com>
2022-12-28 01:24:00 +0900

    chore: stiches 제외, scss 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles'), path.join(__dirname, 'src/components')],
+  },
 }
 
 module.exports = nextConfig
