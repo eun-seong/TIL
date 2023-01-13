e09c115f59dc9fc9e92083280f652bf7e05765a1
eun-seong <eunseong.dev@gmail.com>
2023-01-13 23:15:49 +0900

    feat: home에 안내 문구 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import { styled } from 'styles/theme'
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Description>
+      방문해 주셔서 감사합니다🙇‍♀️
+      <br />
+      블로그는 단장 중이에요🥹
+      <br />
+      꾸준히 만들어 나갈 예정이니, 많관부🪴
+    </Description>
+  )
 }
+
+const Description = styled('div', {
+  textAlign: 'center',
+  fontSize: '1.2rem',
+  paddingTop: 50,
+  lineHeight: '2rem',
+})
